6198aaea83e89e010b9790851735bf049c1fe449
IlSense98 <f.sensenhauser@gmail.com>
2024-03-25 22:38:40 +0100

    Second Commit
    almost

---
+++ Assets/Scenes/SpawnObstacles.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float maxx;
+    public float minx; public float maxY;
+    public float minY;
+    public float timeBetweenSpawn;
+    private float spawnTime;
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        if (Time.time > spawnTime)
+    {
+
+            Spawn();
+            spawnTime = Time.time + timeBetweenSpawn;
+
+        }
+
+
+    }
+
+    void Spawn()
+
+    {
+        float randomX = Random.Range(minx, maxY);
+        float randomy = Random.Range(minx, maxY);
+
+        Instantiate(obstacle, transform.position + new Vector3(randomX, randomy, 0), transform.rotation);
+
+    }
+
+
+}
+
+
